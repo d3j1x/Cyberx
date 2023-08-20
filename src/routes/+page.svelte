77041716ec52ea751482fd2fdf456e7778346f11
d3j1x <d3j1x@yahoo.com>
2023-08-20 12:03:08 +0100

    add quote

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import { Alert } from 'flowbite-svelte';
+	import { Blockquote } from 'flowbite-svelte';
+  	import { Icon } from 'flowbite-svelte-icons';
 	import Terminal from '$lib/components/Terminal.svelte';
 </script>
 
@@ -8,7 +10,17 @@
 	<meta name="description" content="Cyber Security | Secrets - Resources - Hints - Tips ..." />
 </svelte:head>
 
-<div class="flex mt-16 justify-center items-center">
+
+<div class="flex justify-center items-center p-8">
+	<Blockquote size="xl" >
+		<Icon name="quote-solid" class="justify-center items-center w-10 h-10 text-gray-400 dark:text-gray-600" />
+		<span>"Cyber X Is Just Awesome Opensource.
+			<br>Best Cybersecurity Tips Hint Source..."</span>
+	</Blockquote>
+</div>
+
+
+<div class="flex mt-5 justify-center items-center">
 	<!-- <h1 class="text-3xl font-semibold">Welcome to CyberX.</h1> -->
 	<Terminal />
 </div>
