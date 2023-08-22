b6f00ad559ed9c6854cdba4953ba79691ab867b6
d3j1x <d3j1x@yahoo.com>
2023-08-22 19:28:28 +0100

    add cybersecurity path way

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 	import { Blockquote } from 'flowbite-svelte';
   	import { Icon } from 'flowbite-svelte-icons';
 	import Terminal from '$lib/components/Terminal.svelte';
+	import { Card, Button } from 'flowbite-svelte';
+	import pathseek from '$lib/img/pathway.png';
+  	
 </script>
 
 <svelte:head>
@@ -23,6 +26,20 @@
 <div class="flex mt-3 justify-center items-center">
 	<!-- <h1 class="text-3xl font-semibold">Welcome to CyberX.</h1> -->
 	<Terminal />
+</div>
+
+<div class="flex my-10 justify-center items-center">
+	<Card img={pathseek} reverse={true} class="mb-4">
+	  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CYBERSECURITY CAREER PATHWAY
+	</h5>
+	  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">There are many opportunities for workers to start and advance their careers within cybersecurity...</p>
+	  <a href="https://www.cyberseek.org/pathway.html" target="_blank" rel="noreferrer">
+		<Button>
+		Read more <Icon name="arrow-right-outline" class="w-3.5 h-3.5 ml-2 text-white" />
+	  </Button>
+	  </a>
+	</Card>
+	
 </div>
 
 
